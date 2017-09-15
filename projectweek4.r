8414292610021be8e8e5d81e60a80b8e0ce14725
sakhinanasagarsrinivas <sagarsrinivastcs@gmail.com>
2017-09-15 11:02:13 +0530

    Assignment Solutions

--- projectweek4.r
+++ projectweek4.r
@@ -13,11 +13,11 @@ unzip(zipfile = "/home/sagar/Dataset.zip")
 ## Read .txt files into R
 # readme files identifies which files should be used in dataset
 
-activitytest  <- read.table("/home/sagarUCI/HAR Dataset/test/Y_test.txt",header = FALSE)
-activitytrain  <- read.table("/home/sagarUCI/HAR Dataset/train/Y_train.txt",header = FALSE)
-subjecttrain <- read.table("/home/sagarUCI/HAR Dataset/train/subject_train.txt",header = FALSE)
-subjecttest <- read.table("/home/sagarUCI/HAR Dataset/test/subject_test.txt",header = FALSE)
-featurestest  <- read.table("/home/sagarUCI/HAR Dataset/test/x_test.txt",header = FALSE)
+activitytest  <- read.table("/home/sagarUCI HAR Dataset/test/Y_test.txt",header = FALSE)
+activitytrain  <- read.table("/home/sagar/UCI HAR Dataset/train/Y_train.txt",header = FALSE)
+subjecttrain <- read.table("/home/sagar/UCI HAR Dataset/train/subject_train.txt",header = FALSE)
+subjecttest <- read.table("/home/sagar/UCI HAR Dataset/test/subject_test.txt",header = FALSE)
+featurestest  <- read.table("/home/sagar/UCI HAR Dataset/test/x_test.txt",header = FALSE)
 featurestrain  <- read.table("/home/sagar/UCI HAR Dataset/train/x_train.txt",header = FALSE)
 
 ## Merge data into one data set
